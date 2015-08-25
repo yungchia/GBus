25143ca340775a0c8942e6c6ab645fff4241b729
George Lin <georgelin@oneplus.tw>
2015-08-25 18:10:56 +0800

    Update change 0825

--- parse.cpp
+++ parse.cpp
@@ -2,6 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
+//struct {
+
+//}
+
 int main(int argc, char* const argv[]) {
-	return 0;
+    printf("input %d %s\n", argc, argv[1]);
+
+    if (argc == 2) {
+        FILE* pFile = NULL;
+        pFile = fopen(argv[1],"r");
+
+        char* mystring;
+        char myline[200];
+        while(!feof(pFile)) {
+            if (fgets(myline, 200, pFile) != NULL) {
+                mystring = strtok(myline, "\0");
+                //printf("%ld\n", strlen(mystring));
+                for (int i=0; i<strlen(mystring); i++) {
+                    if (strncmp(mystring,"南港",6) == 0) {
+                        puts(mystring);
+                        break;
+                    }
+                }
+                puts(mystring);
+            }
+        }
+
+        fclose(pFile);
+    }
+    return 0;
 }
